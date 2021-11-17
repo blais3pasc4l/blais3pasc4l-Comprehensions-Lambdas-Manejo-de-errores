fe895b433f5b5c6916994e9e8dfded8bee82b1ed
blais3pasc4l <juandavidcalderonpena@gmail.com>
2021-11-16 21:04:08 -0500

    apuntes y resolucion clara de ambas formas

--- filtro_de_datos.py
+++ filtro_de_datos.py
@@ -76,8 +76,10 @@ DATA = [
 def run():
     all_python_devs = [worker['name'] for worker in DATA if worker['language'] == 'python']
     all_workers = [worker['name'] for worker in DATA if worker['organization'] == 'Platzi']
-
-    for worker in all_workers:
+    adults = list(filter(lambda worker: worker['age'] > 18, DATA))
+    adults = list(map(lambda worker: worker['name'], adults))
+    old_people = list(map(lambda worker: worker | {'old': worker['age'] > 70}, DATA))
+    for worker in old_people:
         print(worker)
 
 if __name__ == '__main__':
